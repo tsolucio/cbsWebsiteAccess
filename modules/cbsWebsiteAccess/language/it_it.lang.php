976d0a61c8e4362b5d78669dc712e23698bfb6a0
= <=>
2014-11-27 16:39:40 +0100

    Website Access

---
+++ modules/cbsWebsiteAccess/language/it_it.lang.php
@@ -0,0 +1,20 @@
+<?php
+$mod_strings = array (
+//label of the distributor module
+    'cbsWebsiteAccess' => 'Website Accesso',
+    'cbsWebsiteAccess' => 'Website Accesso',
+    'websiteaccessno' => 'Website Accesso No.',
+    'access_date' => 'Accesso Data',
+    'access_time' => 'Accesso Tempo',
+    'relid' => 'Relato a',
+    'access_url' => 'Accesso URL',
+    'access_web' => 'Accesso Web',
+    'sitevisit' => 'Site Visit',
+
+    'LBL_CUSTOM_INFORMATION' => 'Informazioni Personalizzate',
+    'LBL_CBSWEBSITEACCESS_INFORMATION' => 'Informazioni Website Accesso',
+    'LBL_DESCRIPTION_INFORMATION' => 'Informazioni Descrizione',
+ //end
+);
+
+?>
