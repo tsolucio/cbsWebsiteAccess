e0195d4cc1fdaae0ecf13a98916fdc883e726568
Joe Bordes <joe@tsolucio.com>
2014-12-06 19:07:01 +0100

    related list on campaigns

--- modules/cbsWebsiteAccess/cbsWebsiteAccess.php
+++ modules/cbsWebsiteAccess/cbsWebsiteAccess.php
@@ -441,18 +441,17 @@ class cbsWebsiteAccess extends CRMEntity {
 	 */
 	function vtlib_handler($modulename, $event_type) {
 		if($event_type == 'module.postinstall') {
-            $modAccounts=Vtiger_Module::getInstance('Accounts');
+			$modAccounts=Vtiger_Module::getInstance('Accounts');
 			$modContacts=Vtiger_Module::getInstance('Contacts');
-            $modLeads=Vtiger_Module::getInstance('Leads');
-            $module=Vtiger_Module::getInstance('cbsWebsiteAccess');
-                        
-            if ($modAccounts) $modAccounts->setRelatedList($module, 'cbsWebsiteAccess', Array('ADD'),'get_dependents_list');
+			$modLeads=Vtiger_Module::getInstance('Leads');
+			$module=Vtiger_Module::getInstance('cbsWebsiteAccess');
+			if ($modAccounts) $modAccounts->setRelatedList($module, 'cbsWebsiteAccess', Array('ADD'),'get_dependents_list');
 			if ($modContacts) $modContacts->setRelatedList($module, 'cbsWebsiteAccess', Array('ADD'),'get_dependents_list');
 			if ($modLeads) $modLeads->setRelatedList($module, 'cbsWebsiteAccess', Array('ADD'),'get_dependents_list');
-			
-                    
+			$modCmps=Vtiger_Module::getInstance('Campaigns');
+			if ($modCmps) $modCmps->setRelatedList($module, 'cbsWebsiteAccess', Array('ADD'),'get_dependents_list');
 			// TODO Handle post installation actions
-			$this->setModuleSeqNumber('configure', $modulename, $modulename.'-', '0000001');
+			$this->setModuleSeqNumber('configure', $modulename, 'cbsWebsite-', '0000001');
 		} else if($event_type == 'module.disabled') {
 			// TODO Handle actions when this module is disabled.
 		} else if($event_type == 'module.enabled') {
